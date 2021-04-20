2f21ef5d644875ac1bb911b7215ef7fe8cbd31f8
Vinícius Gomes <vinigc19@gmail.com>
2021-04-20 15:17:48 -0300

    First Commit

---
+++ src/controllers/SettingsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+class SettingsController {
+    async create(request: Request, response: Response) {
+        const { chat, username } = request.body;
+
+        const settingRepository = getCustomRepository(SettingsRepository);
+    
+        const settings = settingRepository.create({
+            chat,
+            username,
+        });
+    
+        await settingRepository.save(settings);
+    
+        return response.json(settings);
+    }
+}
+
+export { SettingsController };
