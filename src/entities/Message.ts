819c39521a8c3b424094a936f339fbfcf2dd965e
Vinícius Gomes <vinigc19@gmail.com>
2021-04-26 10:29:56 -0300

    Create Services, Repositorys, Entities and Controllers the another columns in database

---
+++ src/entities/Message.ts
@@ -0,0 +1,34 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { v4 } from 'uuid';
+import { User } from "./User";
+
+@Entity("messages")
+class Message {
+    
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    admin_id: string;
+
+    @Column()
+    text: string;
+    
+    @JoinColumn({ name: "user_id" })
+    @ManyToOne(() => User)
+    user: User;
+
+    @Column()
+    user_id: string;
+
+    @CreateDateColumn()
+    created_at: string;
+
+    constructor() {
+        if (!this.id) {
+            this.id = v4();
+        }
+    }
+}
+
+export { Message };
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import { MessagesController } from './controllers/MessagesController';
 import { SettingsController } from './controllers/SettingsController';
+import { UsersController } from './controllers/UsersController';
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const messagesController = new MessagesController();
+const usersController = new UsersController();
 
+routes.get('/messages/:id', messagesController.showByUser);
+routes.post('/messages', messagesController.create);
 routes.post('/settings', settingsController.create);
+routes.post('/users', usersController.create);
 
 export { routes };
